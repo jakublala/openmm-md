53a4d91b76df09052560cc96cd29fe5701afe341
jakublala <jakublala@gmail.com>
2024-07-17 10:45:46 +0200

    hbonds

--- stability.py
+++ stability.py
@@ -30,7 +30,7 @@ def main(
         topology=pdb.topology, 
         nonbondedMethod=PME,
         nonbondedCutoff=1*nanometer,
-        constraints=None
+        constraints=HBonds
         )
     print('Periodic boundary conditions:', system.usesPeriodicBoundaryConditions())
 
