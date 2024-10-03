00e7199c7ea75d902bbc8a08ada513394f67f694
jakublala <jakublala@gmail.com>
2024-10-03 15:28:23 +0100

    new things with OPES for hx1

--- plumed/run_plumed.py
+++ plumed/run_plumed.py
@@ -22,12 +22,12 @@ constraintTolerance = 10**(-4) #Quite a default value for accuracy
 
 # Integration Options
 fs = 0.001 * picoseconds
-dt = 4 * fs
+dt = 4 * fs / 2
 temperature = 300*kelvin
 friction = 1.0/picosecond
 
 # Simulation Options
-steps = 10**7
+steps = 5 * 10**7
 equilibrationSteps = 10**5
 # platform = Platform.getPlatformByName('OpenCL')
 platform = Platform.getPlatformByName('CUDA')
@@ -35,6 +35,10 @@ platform = Platform.getPlatformByName('CUDA')
 
 # TODO: employ this later on with 100 ps trajectory
 traj_interval = int(100 * picoseconds / dt)
+
+
+# TODO: maybe add an option to not log waters
+# non_water_ion_atoms_indices from ../stability.py
 
 dcdReporter = DCDReporter(
 	'traj.dcd', 
@@ -44,8 +48,7 @@ dcdReporter = DCDReporter(
 
 dataReporter = StateDataReporter(
             file='logger',
-            # reportInterval=10000,
-            reportInterval=1000,
+            reportInterval=10000,
             step=True,
             time=True,
             potentialEnergy=True,
@@ -83,6 +86,23 @@ integrator = LangevinMiddleIntegrator(temperature, friction, dt)
 integrator.setConstraintTolerance(constraintTolerance)
 simulation = Simulation(topology, system, integrator, platform)
 simulation.context.setPositions(positions)
+# Get the box vectors (i.e., box size)
+box_vectors = topology.getPeriodicBoxVectors()
+
+# Print the box vectors
+print("Box Vectors (in nanometers) from topology object:")
+print("x:", box_vectors[0] / unit.nanometers)
+print("y:", box_vectors[1] / unit.nanometers)
+print("z:", box_vectors[2] / unit.nanometers)
+
+# Then get vectors from the simulation object
+box_vectors = system.getDefaultPeriodicBoxVectors()
+
+# Print the box vectors
+print("Box Vectors (in nanometers) from system object:")
+print("x:", box_vectors[0] / unit.nanometers)
+print("y:", box_vectors[1] / unit.nanometers)
+print("z:", box_vectors[2] / unit.nanometers)
 
 # Simulate
 print('Simulating...')
