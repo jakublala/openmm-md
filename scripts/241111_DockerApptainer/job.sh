29b9226401325fb91e3124848a7841fce2e208ac
Jakub Lala <jakublala@gmail.com>
2024-11-12 09:47:45 +0000

    notes on docker + mmm job

--- scripts/241111_DockerApptainer/job.sh
+++ scripts/241111_DockerApptainer/job.sh
@@ -7,10 +7,10 @@
 #$ -l h_rt=0:10:0
 
 # Request 1 gigabyte of RAM (must be an integer followed by M, G, or T)
-#$ -l mem=1G
+#$ -l mem=80G
 
 # Request 15 gigabyte of TMPDIR space (default is 10 GB)
-#$ -l tmpfs=15G
+#$ -l tmpfs=32G
 
 # Set the name of the job.
 #$ -N GPUJob
@@ -18,36 +18,24 @@
 # Set the working directory to somewhere in your scratch space.
 #$ -wd /home/mmm1486/Scratch/openmm-md
 
+#$ -P Free
+#$ -A Imperial_Mat
+
 # Change into temporary directory to run work
 cd $TMPDIR
 
 module load apptainer
 module load cuda/11.8
-apptainer pull docker://jakublala/openmm-md:latest
 
-# Run the application - the line below is just a random example.
-mygpucode
+apptainer pull --force openmm-md.sif docker://jakublala/openmm-md:latest
+
+apptainer exec openmm-md.sif bash -c "pip install -e $HOME/projects/openmm-md && python $HOME/projects/openmm-md/src/plumed/main.py \
+    --filepath 'data/241109_INFconstruct/input/PQ19/PQ19_B30L4.pdb' \
+    --device_index 'nan' \
+    --device 'cuda' \
+    --timestep 2 \
+    --mdtime 5 \
+    --output_dir 'data/241111_DockerApptainer/output'"
 
 # 10. Preferably, tar-up (archive) all output files onto the shared scratch area
 tar zcvf $HOME/Scratch/files_from_job_$JOB_ID.tar.gz $TMPDIR
-
-
-module load anaconda3/personal
-source activate openmm
-module load cuda/11.4.2
-
-# Read input parameters
-INPUT_PATH=${INPUT_PATH}
-OUTPUT_DIR=${OUTPUT_DIR}
-TIMESTEP=${TIMESTEP:-2}      # Default to 2 if not specified
-MDTIME=${MDTIME:-500}        # Default to 500 if not specified
-RESTART_RFILE=${RESTART_RFILE:-None}
-
-python ../../src/plumed/main.py \
-    --filepath "../../$INPUT_PATH" \
-    --device_index "nan" \
-    --device "cuda" \
-    --timestep $TIMESTEP \
-    --mdtime $MDTIME \
-    --restart_rfile $RESTART_RFILE \
-    --output_dir "../../$OUTPUT_DIR"
