d62d62985fdb268a8712795a5242e97f6f65a06b
Jakub Lala <jakublala@gmail.com>
2024-11-15 15:45:32 +0000

    changes INF

--- scripts/241115_FuB_CD28Focus/submit.sh
+++ scripts/241115_FuB_CD28Focus/submit.sh
@@ -1,53 +1,29 @@
 #!/bin/bash
 
 submit_simulation() {
-    local input_path=$1
-    local output_dir=$2
-    local job_name=$3
-    local timestep=${4:-2}
-    local mdtime=${5:-500}
-    local restart_rfile=${6:-None}
-    local barrier=${7:-200}
-    local padding=${8:-5}
-    local upper_wall=${9:-5}
+    local system=$1
+    local padding=${2:-5}
+    local upper_wall=${3:-5}
 
-    qsub -N "$job_name" \
-        -v "INPUT_PATH=$input_path,OUTPUT_DIR=$output_dir,TIMESTEP=$timestep,MDTIME=$mdtime,RESTART_RFILE=$restart_rfile,BARRIER=$barrier,PADDING=$padding,UPPER_WALL=$upper_wall" \
+    qsub -N "$system" \
+        -v "SYSTEM=$system,PADDING=$padding,UPPER_WALL=$upper_wall" \
         template.pbs
 }
 
 # CD28-G-UW5P2 (5nm upper wall, 2nm padding)
-submit_simulation "data/241010_FoldingUponBinding/input/CD28/CD28_general.pdb" \
-                 "data/241010_FoldingUponBinding/output/CD28-G-UW5P2/241115" \
-                 "cd28_uw5p2" \
-                 2 \
-                 500 \
-                 "None" \
-                 200 \
-                 2 \
-                 5
+submit_simulation "CD28-G-UW5P2" \
+                2 \
+                5
 
 # CD28-G-UW5P4 (5nm upper wall, 4nm padding)
-submit_simulation "data/241010_FoldingUponBinding/input/CD28/CD28_general.pdb" \
-                 "data/241010_FoldingUponBinding/output/CD28-G-UW5P4/241115" \
-                 "cd28_uw5p4" \
-                 2 \
-                 500 \
-                 "None" \
-                 200 \
-                 4 \
-                 5
+submit_simulation "CD28-G-UW5P4" \
+                4 \
+                5
 
 # CD28-G-UW8P4 (8nm upper wall, 4nm padding)
-submit_simulation "data/241010_FoldingUponBinding/input/CD28/CD28_general.pdb" \
-                 "data/241010_FoldingUponBinding/output/CD28-G-UW8P4/241115" \
-                 "cd28_uw8p4" \
-                 2 \
-                 500 \
-                 "None" \
-                 200 \
-                 4 \
-                 8
+submit_simulation "CD28-G-UW8P4" \
+                4 \
+                8
 
 # python ../../src/plumed/main.py \
 #     --filepath "../../data/241010_FoldingUponBinding/input/CD28/CD28_general.pdb" \
--- scripts/241115_INFconstruct/README.md
+++ scripts/241115_INFconstruct/README.md
@@ -1,2 +1,6 @@
 ### 241111
 First, let's do just simple MD on the Q7 and Z1 systems.
+
+
+### 241115
+Now focusing on the Z1-B50W system.
