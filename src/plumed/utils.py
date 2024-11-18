90388899fdd678193f760c58592eaeb4a6817e41
Jakub Lala <jakublala@gmail.com>
2024-11-18 11:16:11 +0000

    bugfix on plumed IDs

--- src/plumed/utils.py
+++ src/plumed/utils.py
@@ -1,3 +1,8 @@
 from openmm.unit import nanoseconds, picoseconds
 def get_checkpoint_interval(timestep):
     return int((1 * nanoseconds) / (timestep * 0.001 * picoseconds)) 
+
+
+import MDAnalysis as mda
+def get_atom_ids_from_chain(chain_id: str, filename: str, output_dir: str):
+    return mda.Universe(f'{output_dir}/{filename}_fixed.pdb').select_atoms(f'chainid {chain_id}').ids
