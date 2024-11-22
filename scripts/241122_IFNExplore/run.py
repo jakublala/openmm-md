6d0db527e6cbe0fb67594d5ab8a4325b2defd4f6
Jakub Lala <jakublala@gmail.com>
2024-11-22 20:33:40 +0000

    testing IFN sweep

--- scripts/241122_IFNExplore/run.py
+++ scripts/241122_IFNExplore/run.py
@@ -1,8 +1,24 @@
 from src.plumed.main import main
 from src.plumed.utils import get_checkpoint_interval
-import mda
+import MDAnalysis as mda
 from src.models import Residue, Segment
 import fire
+import re
+
+def extract_from_system(system):
+    # need to use regex to extract for instance
+    # Q7-B30L4 -> BINDER_LENGTH = 30, LINKER1_LENGTH = 4, LINKER2_LENGTH = 4
+    # Z1-B40L10W -> BINDER_LENGTH = 40, LINKER1_LENGTH = 10, LINKER2_LENGTH = 10
+    # PQ19-B50L10W -> BINDER_LENGTH = 50, LINKER1_LENGTH = 10, LINKER2_LENGTH = 10 
+    pattern = r'([A-Z0-9]+)-B(\d+)L(\d+)W?'  # Made W optional with ? and added digits to first group
+    match = re.match(pattern, system)
+    if match:
+        BINDER_LENGTH = int(match.group(2))
+        LINKER1_LENGTH = int(match.group(3))
+        LINKER2_LENGTH = LINKER1_LENGTH
+    else:
+        raise ValueError(f"System {system} does not match pattern {pattern}")
+    return BINDER_LENGTH, LINKER1_LENGTH, LINKER2_LENGTH
 
 def run(filepath, system):
     DATE = '241122'
@@ -14,12 +30,14 @@ def run(filepath, system):
     MDTIME = 50
 
     # 1. CREATE PLUMED INPUT
-
-    BINDER_LENGTH = 50
-    LINKER1_LENGTH = 10
-    PROTEASE_LENGTH = 10
-    LINKER2_LENGTH = 10
+    if 'Q7' in system:
+        PROTEASE_LENGTH = 9
+    else:
+        PROTEASE_LENGTH = 10
+    BINDER_LENGTH, LINKER1_LENGTH, LINKER2_LENGTH = extract_from_system(system)
     INF_LENGTH = 161
+
+    print(f"{LINKER1_LENGTH=}, {LINKER2_LENGTH=}, {BINDER_LENGTH=}, {system}")
 
     NON_INF_LENGTH = BINDER_LENGTH + LINKER1_LENGTH + PROTEASE_LENGTH + LINKER2_LENGTH
 
@@ -28,6 +46,14 @@ def run(filepath, system):
         NON_INF_LENGTH + INF_LENGTH
         ), f"Expected {NON_INF_LENGTH + INF_LENGTH} residues, got {nresidues}"
 
+    if 'W' in system:
+        # this means a wider binding spot, 55 to 135
+        spot2_start = 55
+        spot2_end = 135
+    else:
+        # this means the default binding spot, 80 to 100
+        spot2_start = 80
+        spot2_end = 100
 
     # 1 - indexed
     spot1_residues = Segment(residues=[
@@ -45,13 +71,13 @@ def run(filepath, system):
             chain_id='A',
             indexing=1
         ) for i in range(
-            NON_INF_LENGTH + 55 + 1,
-            NON_INF_LENGTH + 135 + 1
+            NON_INF_LENGTH + spot2_start + 1,
+            NON_INF_LENGTH + spot2_end + 1
             )
     ])
 
-    # upper_wall_at = (3.8 * (LINKER1_LENGTH + PROTEASE_LENGTH + LINKER2_LENGTH)) / 10 / 2
-    # padding = upper_wall_at + 1
+    upper_wall_at = (3.8 * (LINKER1_LENGTH + PROTEASE_LENGTH + LINKER2_LENGTH)) / 10 / 2
+    padding = upper_wall_at + 1
 
     config = {
         'type': 'opes-explore',
@@ -62,7 +88,7 @@ def run(filepath, system):
         'cutoff': 0.8,
         'restart_rfile': None,
         'state_wstride': get_checkpoint_interval(TIMESTEP),
-        'upper_wall.at': 13,
+        'upper_wall.at': upper_wall_at,
         'upper_wall.exp': 6,
         'upper_wall.kappa': 1000.0,
         'spot1_residues': spot1_residues,
@@ -76,21 +102,24 @@ def run(filepath, system):
     else:
         gpu_indices = None
 
-    # 2. RUN MINIMIZATION AND SIMULATION
-    main(
-        filepath=FILEPATH,
-        output_dir=OUTPUT_DIR,
-        temperature=TEMPERATURE,
-        mdtime=MDTIME,
-        timestep=TIMESTEP,
-        device_index=gpu_indices,
-        device='cuda',
-        split_chains=False,
-        logging_frequency=LOGGING_FREQUENCY,
-        config=config,
-        padding=padding,
-        chain_mode='single-chain'
-    )
+    print(f"{LINKER1_LENGTH=}, {LINKER2_LENGTH=}, {BINDER_LENGTH=}, {system}, {spot2_start=}, {spot2_end=}")
+    # assert 0 == 1
+
+    # # 2. RUN MINIMIZATION AND SIMULATION
+    # main(
+    #     filepath=FILEPATH,
+    #     output_dir=OUTPUT_DIR,
+    #     temperature=TEMPERATURE,
+    #     mdtime=MDTIME,
+    #     timestep=TIMESTEP,
+    #     device_index=gpu_indices,
+    #     device='cuda',
+    #     split_chains=False,
+    #     logging_frequency=LOGGING_FREQUENCY,
+    #     config=config,
+    #     padding=padding,
+    #     chain_mode='single-chain'
+    # )
 
 
 
---
+++ scripts/241122_IFNExplore/test.sh
@@ -0,0 +1,21 @@
+python run.py --filepath "../../data/241109_INFconstruct/input/Q7-B30L4.pdb" --system Q7-B30L4
+python run.py --filepath "../../data/241109_INFconstruct/input/Q7-B30L7.pdb" --system Q7-B30L7
+python run.py --filepath "../../data/241109_INFconstruct/input/Q7-B30L10.pdb" --system Q7-B30L10
+python run.py --filepath "../../data/241109_INFconstruct/input/Q7-B40L10.pdb" --system Q7-B40L10
+python run.py --filepath "../../data/241109_INFconstruct/input/Q7-B40L10W.pdb" --system Q7-B40L10W
+python run.py --filepath "../../data/241109_INFconstruct/input/Q7-B50L10W.pdb" --system Q7-B50L10W
+
+python run.py --filepath "../../data/241109_INFconstruct/input/PQ19-B30L4.pdb" --system PQ19-B30L4
+python run.py --filepath "../../data/241109_INFconstruct/input/PQ19-B30L7.pdb" --system PQ19-B30L7
+python run.py --filepath "../../data/241109_INFconstruct/input/PQ19-B30L10.pdb" --system PQ19-B30L10
+python run.py --filepath "../../data/241109_INFconstruct/input/PQ19-B40L10.pdb" --system PQ19-B40L10
+python run.py --filepath "../../data/241109_INFconstruct/input/PQ19-B40L10W.pdb" --system PQ19-B40L10W
+python run.py --filepath "../../data/241109_INFconstruct/input/PQ19-B50L10W.pdb" --system PQ19-B50L10W
+
+python run.py --filepath "../../data/241109_INFconstruct/input/Z1-B30L4.pdb" --system Z1-B30L4
+python run.py --filepath "../../data/241109_INFconstruct/input/Z1-B30L7.pdb" --system Z1-B30L7
+python run.py --filepath "../../data/241109_INFconstruct/input/Z1-B30L10.pdb" --system Z1-B30L10
+python run.py --filepath "../../data/241109_INFconstruct/input/Z1-B40L10.pdb" --system Z1-B40L10
+python run.py --filepath "../../data/241109_INFconstruct/input/Z1-B40L10W.pdb" --system Z1-B40L10W
+python run.py --filepath "../../data/241109_INFconstruct/input/Z1-B50L10W.pdb" --system Z1-B50L10W
+
